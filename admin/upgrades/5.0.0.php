646d3e198971f10ea6c0b36aa84aa06751744ca1
lsces <lester@lsces.co.uk>
2025-08-27 16:26:14 +0100

    Tidy old upgrade stuff for new 5.0 start

---
+++ admin/upgrades/5.0.0.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ */
+global $gBitInstaller;
+
+$infoHash = array(
+	'package'      => WIKI_PKG_NAME,
+	'version'      => str_replace( '.php', '', basename( __FILE__ )),
+	'description'  => "Fixed new format updates",
+	'post_upgrade' => NULL,
+);
+
+
